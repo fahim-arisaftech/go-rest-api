f3847acb070240cf175f3a31dc3136f2cc767d0c
fahim chowdhury <fahim.arisaftech@gmail.com>
2023-05-30 10:44:07 +0600

    feat: boilerplate for http server ready

---
+++ handler_readiness.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, 200, struct{}{})
+}
+
+func handleErr(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
---
+++ json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5xx error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
